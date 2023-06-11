ea396a64d92814685753fdc1e85bdb1ca83a84fd
Yousef Elsayed <yousef4511@outlook.com>
2023-06-12 02:21:21 +0300

    Join book interface and implementation in one file

--- book.hpp
+++ book.hpp
@@ -1,8 +1,10 @@
 #ifndef BOOK_6122023_H
 #define BOOK_6122023_H
 
+#include <iostream>
 #include <vector>
 #include <string>
+
 
 class Book {
     private:
@@ -13,24 +15,91 @@ class Book {
         std::vector<std::string> pagesToContent;
     
     public:
-        Book();
+        Book(){
+           title = authorName = "";
+            isbn =  numOfPages = -1;
+        }
+
         // Setters
-        void setISBN(int isbn_);
-        void setTitle(std::string title_);
-        void setAuthorName(std::string authorName_);
-        void setNumOfPages(int numOfPages_);
-        void setContent(const std::vector<std::string>& pagesToContent_);
+        void setISBN(int isbn_){
+            isbn = isbn_;
+        }
+
+        void setTitle(std::string title_){
+            title = title_;
+        }
+        
+        void setAuthorName(std::string authorName_) {
+            authorName = authorName_;
+        }
+        
+        void setNumOfPages(int numOfPages_){
+            numOfPages = numOfPages_;
+        }
+        
+        void setContent(const std::vector<std::string>& pagesToContent_){
+            for (auto& content : pagesToContent_)
+                pagesToContent.push_back(content);
+        }
 
         // Getters
-        int getISBN() const;
-        std::string getTitle() const ;
-        std::string getAuthorName() const ;
-        int getNumOfPages() const ;
-        const std::vector<std::string>& getContent() const;
+        int getISBN() const{
+            return isbn;
+        }
+        
+        std::string getTitle() const {
+            return title;
+        }
+        
+        std::string getAuthorName() const {
+            return authorName;
+        }
+        
+        int getNumOfPages() const {
+            return numOfPages;
+        }
+        
+        const std::vector<std::string>& getContent() const{
+            return pagesToContent;
+        }
 
         // Other
-        void print();
-        void readBook(int isbn);
+        void print(){
+            std::cout << "\n" << "ISBN : " << getISBN()
+                << "\n" << "Title : " << getTitle()
+                << "\n" << "Author : " << getAuthorName()
+                << "\n" << "Number of Pages : " << getNumOfPages() << "\n";
+        }
+        
+        void readBook(int isbn){
+            setISBN(isbn);
+
+            std::cout << "Enter Title : ";
+            std::string title;
+            std::cin >> title;
+            setTitle(title);
+
+            std::cout << "Enter Author Name : ";
+            std::string name;
+            std::cin >> name;
+            setAuthorName(name);
+
+            std::cout << "How many pages : ";
+            int count;
+            std::cin >> count;
+            setNumOfPages(count);
+
+            std::vector<std::string> bookContent;
+            for (int i {1}; i <= count; i++) {
+                std::cout << "Enter page #" << i << " : ";
+                std::string content;
+                std::cin >> content;
+                bookContent.push_back(content);
+            }
+            setContent(bookContent);
+
+            return;
+        }
 };
 
 #endif

7afc250f2ad4c4c620d69b3e0538bd4a2ec5ed67
Yousef Elsayed <yousef4511@outlook.com>
2023-06-19 21:14:05 +0300

    Merge user interface and implementation into 1 hpp

--- user.hpp
+++ user.hpp
@@ -2,6 +2,8 @@
 #define USER_6122023_H
 
 #include <string>
+#include <iostream>
+#include <utility>
 
 class User
 {
@@ -14,29 +16,102 @@ private:
     bool isAdmin {0};
 
 public:
-    User();
+    User() = default;
 
-    User(int id, std::string userName, std::string name, std::string email, std::string password, bool isAdmin);
+    User(int id, std::string userName, std::string name, std::string email, std::string password, bool isAdmin) : id{id}, userName{userName}, name{name}, email{email}, password{password}, isAdmin{isAdmin}
+    {
+    }
 
     // Getters
-    int getId() const;
-    std::string getUserName() const ;
-    std::string getName() const ;
-    std::string getEmail() const ;
-    std::string getPassword() const ;
-    bool getIsAdmin() const ;
+    int getId() const
+    {
+      return id;
+    }
+
+    std::string getUserName() const
+    {
+      return userName;
+    }
+
+    std::string getName() const
+    {
+      return name;
+    }
+
+    std::string getEmail() const
+    {
+      return email;
+    }
+
+    std::string getPassword() const
+    { 
+      return password;
+    }
+
+    bool getIsAdmin() const
+    {
+      return isAdmin;
+    }
 
     // Setters
-    void setId(int id);
-    void setUserName(std::string userName);
-    void setName(std::string name);
-    void setEmail(std::string email);
-    void setPassword(std::string password);
-    void setIsAdmin(bool isAdmin);
+    void setId(int id_)
+    {
+      id = id_;
+    }
 
-    void readUser(std::string userName, int id);
+    void setUserName(std::string userName_)
+    {
+      userName = std::move(userName_);
+    }
 
-    void print() const;
+    void setName(std::string name_)
+    {
+      name = std::move(name_);
+    }
+
+    void setEmail(std::string email_)
+    {
+      email = std::move(email_);
+    }
+
+    void setPassword(std::string password_)
+    {
+      password = std::move(password_);
+    }
+
+    void setIsAdmin(bool isAdmin_)
+    {
+      isAdmin = isAdmin_;
+    }
+
+    void readUser(std::string userName, int id)
+    {
+      setId(id);
+      setUserName(std::move(userName));
+      setIsAdmin(0);
+
+      std::string str;
+      std::cout << "Enter your name (no spaces) : ";
+      std::cin >> str;
+      setName(std::move(str));
+
+      std::cout << "Enter your email (no spaces) : ";
+      std::cin >> str;
+      setEmail(std::move(str));
+
+      std::cout << "Enter your password (no spaces) : ";
+      std::cin >> str;
+      setPassword(std::move(str));
+    }
+
+    void print() const
+    {
+      std::cout << "\nName : " << getName();
+      std::cout << "\nusername : " << getUserName();
+      std::cout << "\nEmail : " << getEmail();
+      std::cout << "\nPassword : " << getPassword();
+      std::cout << "\nisAdmin : " << (getIsAdmin() ? "yes" : "no") << "\n";
+    }
 };
 
 #endif
